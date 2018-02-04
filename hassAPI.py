178fef3d02f2893ed4b72d40b79b1a6bb5e590d3
sordhlm <35914887+sordhlm@users.noreply.github.com>
2018-02-04 22:34:27 +0800

    Update hassAPI.py

--- hassAPI.py
+++ hassAPI.py
@@ -1,30 +1,20 @@
 from requests import get
 from requests import post
 import logging, json, time
+import re
 
-class hassAPI(object):
+
+class hassIF(object):
 
     def __init__(self, ip='192.168.1.108', passwd='Pian#0919'):
         self.url = 'http://%s:8123/api/' % ip
-        self.head = {'x-ha-access': '%s' % passwd,'content-type': 'application/json'
-           }
+        self.head = {'x-ha-access': '%s' % passwd,'content-type': 'application/json'}
 
     def _getRes(self, url):
         return json.loads(get(url, headers=self.head).text)
 
     def _postRes(self, url, data):
         return json.loads(post(url, data=json.dumps(data), headers=self.head).text)
-
-    def turnOn(self, id, red=255, green=100, blue=100, bright=200):
-        url = self.url + 'services/light/turn_on'
-        data = {'entity_id': '%s' % id,'rgb_color': [red, green, blue],'brightness': bright}
-        return self._postRes(url, data)
-
-    def turnOff(self, id):
-        url = self.url + 'services/light/turn_off'
-        data = {'entity_id': '%s' % id}
-        return self._postRes(url, data)
-
     def getLive(self):
         return self._getRes(self.url)
 
@@ -56,22 +46,77 @@ class hassAPI(object):
         url = self.url + 'error_log'
         return self._getRes(url)
 
+
+class hass_light(hassIF):
+    def __init__(self,id, att = None):
+        super(hass_light,self).__init__()
+        self.id = id
+        if att != None:
+            self.att = att
+        self._initAttr()
+    def _initAttr(self):
+        if 'red' not in self.att.keys():
+            self.att['red'] = 100
+        if 'green' not in self.att.keys():
+            self.att['green'] = 100
+        if 'blue' not in self.att.keys():
+            self.att['blue'] = 100
+        if 'bright' not in self.att.keys():
+            self.att['bright'] = 200
+    def updateAttr(self,cmd):
+        logging.debug('%s update attributions'%(self.id))
+        m = re.search('(\w+)([+-]\d+)',cmd)
+        name = m.group(1)
+        act = m.group(2)
+        logging.debug('old %s: %d'%(name,self.att[name]))
+        self.att[name] = eval('%s%s'%(self.att[name],act))
+        if self.att[name] < 0:
+            self.att[name] = 0
+        elif self.att[name] > 255:
+            self.att[name] = 255
+        logging.debug('new %s: %d' % (name, self.att[name]))
+    def turnOn(self):
+        url = self.url + 'services/light/turn_on'
+        data = {'entity_id': '%s' % self.id,'rgb_color': [self.att['red'], self.att['green'], self.att['blue']],
+                'brightness': self.att['bright']}
+        return self._postRes(url, data)
+
+    def turnOff(self):
+        url = self.url + 'services/light/turn_off'
+        data = {'entity_id': '%s' % self.id}
+        return self._postRes(url, data)
+
     def getLightSer(self):
         serv = self._getServ()
         for i in serv:
             if i['domain'] == 'light':
                 return i
 
+class hassAPI(object):
+    def __init__(self):
+        self.light = {}
     def cmdProc(self, cmd):
-        eid = cmd[0]
-        act = cmd[1]
-        if 'turn on' in act:
-            logging.debug('get turn on light cmd')
-            self.turnOn(eid)
-        elif 'turn off' in act:
-            logging.debug('get turn off light cmd')
-            self.turnOff(eid)
+        att = {}
+        att['red'] = 255
+        att['green'] = 0
+        att['blue'] = 0
+        att['bright'] = 200
+        if 'hass_light' in cmd:
+            logging.debug(cmd)
+            cl = cmd.split(' ')
+            eid = 'light.' + cl[2]
+            act = cl[1]
+            if eid not in self.light.keys():
+                logging.debug('new light init')
+                self.light[eid] = hass_light(eid,att)
+            logging.debug('length is %d'%len(cl))
+            if len(cl) > 3:
+                self.light[eid].updateAttr(cl[3])
+            getattr(self.light[eid],act)()
 
+
+    def getEid(self,cmd):
+        m = re.search('')
     def getDevStat(self):
         pass
 
@@ -79,7 +124,15 @@ class hassAPI(object):
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     api = hassAPI()
-    cmd = ['light.Living_Room', 'turn on']
-    for i in range(0, 20):
-        time.sleep(1)
-        api.turnOn('light.Living_Room', 255, 255, i * 10)
+    api.cmdProc('hass_light turnOn livingroom red+10')
+
+    attr = {}
+    attr['red'] = 255
+    attr['green'] = 200
+    attr['blue'] = 0
+    l1 = hass_light('light.livingroom',attr)
+    #l1.turnOn()
+    #cmd = ['light.Living_Room', 'turn on']
+    #for i in range(0, 20):
+    #    time.sleep(1)
+    #    api.turnOn('light.Living_Room', 255, 255, i * 10)
